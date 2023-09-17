ccb4a0aa236e626dd2931c1a1d3f5942a9d66194
AsimGokalp <145282706+AsimGokalp@users.noreply.github.com>
2023-09-17 19:58:54 +0300

    Create main.py
    python dosyası eklendi

---
+++ main.py
@@ -0,0 +1,3 @@
+import random
+gercekler = ["Robotik ve yüzme en sevdiğim hobiler", "Hiç bir evcil hayvanım yok", "otomatik park sisteminde bir kamera,bildirim modülü ve şifre yazma yeri var", "En sevdiğim yemek iskender"]
+print(random.choice(gercekler))
